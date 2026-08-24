316849ea47f05d810042adad86098d5a27ce5a70
Nikolai Matiushev <egao1980@gmail.com>
2026-08-24 17:59:19 +0100

    Encode :false/:true as JSON booleans
    stdio MCP died with YASON:*SYMBOL-ENCODER* when a result contained
    :false. Bind a lowercase symbol encoder as a fallback.

--- tests/protocol-test.lisp
+++ tests/protocol-test.lisp
@@ -11,3 +11,9 @@
     (ok (equal "2.0" (gethash "jsonrpc" msg)))
     (ok (equal "sum" (gethash "method" msg)))
     (ok (= 7 (gethash "id" msg)))))
+
+(deftest encode-false-is-json-boolean
+  (let ((h (make-hash-table :test 'equal)))
+    (setf (gethash "isError" h) :false)
+    (let ((wire (rpc-protocol:encode-response h :id 1)))
+      (ok (search "\"isError\":false" (remove #\space wire))))))
